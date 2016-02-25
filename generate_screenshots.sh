263b3764b2e4dfa10683cbee21d6f2b65afab5cd
Kearabiloe <kea@Proversity.org>
2016-02-25 16:55:37 +0200

    minor fix

--- generate_screenshots.sh
+++ generate_screenshots.sh
@@ -1,7 +1,7 @@
 echo "Generating screenshots...."
 emulator -avd circleci-android21 -no-skin -no-audio -no-window & adb wait-for-device
 adb devices -l
-adb install -r VideoLocker/build/outputs/apk/VideoLocker-prod-release.apk
+adb install -r ~/edx-app-android/VideoLocker/build/outputs/apk/VideoLocker-prod-release.apk
 AppPackage=org.proversity.edx.mobile #Todo: Change package
 cd VideoLocker/src/main/play/en-GB/listing/phoneScreenshots/
 adb shell am start -n $AppPackage/.view.LoginActivity -W
