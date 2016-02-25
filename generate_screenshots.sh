c23a1131c872dee9dc83c397edac198530dd081c
Kearabiloe <kea@Proversity.org>
2016-02-25 13:01:04 +0200

    added placeholders

--- generate_screenshots.sh
+++
@@ -1,5 +0,0 @@
-#Todo:
-adb install -r VideoLocker/build/outputs/apk/VideoLocker-dev-debug.apk
-adb shell am start -n org.edx.mobile/.view.LoginActivity
-adb shell wm size 160x579 density 160
-adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > LoginActivity.png
