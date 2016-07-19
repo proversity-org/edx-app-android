8f5082189ed18e6e164d37a7ce135d79b881ca40
Rene Sorel <rene@proversity.org>
2016-07-19 16:50:22 +0200

    correct packageName for Roboelectric test config

--- VideoLocker/src/test/java/org/edx/mobile/test/BaseTest.java
+++ VideoLocker/src/test/java/org/edx/mobile/test/BaseTest.java
@@ -2,6 +2,7 @@ package org.edx.mobile.test;
 
 import android.support.annotation.NonNull;
 
+import org.edx.mobile.BuildConfig;
 import org.edx.mobile.CustomRobolectricTestRunner;
 import org.edx.mobile.util.observer.AsyncCallableUtils;
 import org.edx.mobile.util.observer.MainThreadObservable;
@@ -9,11 +10,13 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.annotation.Config;
 
 import java.util.concurrent.Executor;
 
 @Ignore
 @RunWith(CustomRobolectricTestRunner.class)
+@Config(constants=BuildConfig.class, packageName="org.edx.mobile")
 public class BaseTest {
     @Before
     public final void beforeBaseTest() {
