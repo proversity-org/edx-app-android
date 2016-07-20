20db86f3d45fd1c71737d7ca4395e7e04a7f46f9
Rene Sorel <rene@proversity.org>
2016-07-20 09:04:13 +0200

    SuppressLint ValidFragment, more memory for gradle on deploy

--- VideoLocker/src/main/java/org/edx/mobile/view/LoginActivity.java
+++ VideoLocker/src/main/java/org/edx/mobile/view/LoginActivity.java
@@ -1,5 +1,6 @@
 package org.edx.mobile.view;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -275,6 +276,7 @@ public class LoginActivity extends PresenterActivity<LoginPresenter, LoginPresen
         return activityLoginBinding.emailEt.getText().toString().trim();
     }
 
+    @SuppressLint("ValidFragment")
     private void showResetPasswordDialog() {
         clearDialogs();
         resetDialog = new ResetPasswordDialog() {
