314d6e01bd683af6e836d23599b283f0473afac4
Rene Sorel <rene@proversity.org>
2017-02-15 15:58:32 +0200

    always create konnekteer endpoint for device token

--- OpenEdXMobile/src/main/java/org/edx/mobile/base/MainApplication.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/base/MainApplication.java
@@ -36,6 +36,7 @@ import org.edx.mobile.module.prefs.PrefManager;
 import org.edx.mobile.module.storage.IStorage;
 import org.edx.mobile.receivers.NetworkConnectivityReceiver;
 import org.edx.mobile.util.Config;
+import org.edx.mobile.util.KonnekteerUtil;
 import org.edx.mobile.util.NetworkUtil;
 import org.edx.mobile.util.NotificationUtil;
 
@@ -90,6 +91,11 @@ public abstract class MainApplication extends MultiDexApplication {
                 (Module) RoboGuice.newDefaultRoboModule(this), (Module) new EdxDefaultModule(this));
 
         injector.injectMembers(this);
+
+        // initialize Konnekteer
+        if (config.getPushNotificationsConfig().isEnabled()) {
+            KonnekteerUtil.createMobileEndpoint(this);
+        }
 
         // initialize Fabric
         if (config.getFabricConfig().isEnabled() && !BuildConfig.DEBUG) {
