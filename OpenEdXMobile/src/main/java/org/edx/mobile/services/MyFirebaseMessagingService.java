124746d11c1186d06c49c6028faa95968484a813
adamkatz <adam.katz@proversity.org>
2018-05-18 12:12:49 +0200

    fixed a bug in firebasemessaging service

---
+++ OpenEdXMobile/src/main/java/org/edx/mobile/services/MyFirebaseMessagingService.java
@@ -0,0 +1,53 @@
+package org.edx.mobile.services;
+
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import org.edx.mobile.R;
+import org.edx.mobile.view.SplashActivity;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        String messageText = remoteMessage.getData().get("default");
+
+        Notification.Builder mBuilder =
+                new Notification.Builder(this)
+                        .setSmallIcon(R.mipmap.small_icon)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                        .setContentTitle(getString(R.string.platform_name))
+                        .setContentText(messageText)
+                        .setStyle(new Notification.BigTextStyle().bigText(messageText));
+
+        Intent resultIntent = new Intent(this, SplashActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(SplashActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(resultPendingIntent)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setPriority(Notification.PRIORITY_HIGH)
+                .setAutoCancel(true);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, mBuilder.build());
+    }
+
+    private void handleNow() {
+        Log.d(TAG, "Short lived task is done.");
+    }
+}
