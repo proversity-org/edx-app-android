f2abc094f63943c977229ea0a3e22f5f922e4e9b
Jose Antonio Gonzalez <antonio@proversity.org>
2018-07-03 09:22:14 +0000

    [Houston] New Android build c54fc9a2-3cda-11e8-b467-0ed5f89f718b

--- OpenEdXMobile/src/main/java/org/edx/mobile/services/MyFirebaseMessagingService.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/services/MyFirebaseMessagingService.java
@@ -25,7 +25,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     Notification.Builder mBuilder =
       new Notification.Builder(this)
-        .setSmallIcon(R.mipmap.ic_launcher)
+        .setSmallIcon(R.mipmap.small_icon)
         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
         .setContentTitle(getString(R.string.platform_name))
         .setContentText(messageText)
